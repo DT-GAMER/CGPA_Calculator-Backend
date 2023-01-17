3810d70977ae4219f40cd6601a2306383c8433db
Korode Ademoroti <122447344+PapaBeer01@users.noreply.github.com>
2023-01-17 10:16:39 +0100

    Update cgpa_calculator.py

--- main/cgpa_calculator.py
+++ main/cgpa_calculator.py
@@ -5,134 +5,133 @@ import numpy as np
 def calculate_gpa(course_units, grades):
     """
     Calculate the GPA given the course units and grades.
-    course_units: list of integers representing the course units
-    grades: list of integers representing the grades
-    return: float rounded to 2 decimal places representing the GPA
     """
-    
-    
     total_cu = sum(course_units)
     weighted_points = np.dot(course_units, grades)
     gpa = weighted_points / total_cu
     return round(gpa, 2)
 
-def calculate_cgpa(level, sem, prev_cgpa, gpa):
+def calculate_cgpa_utme(level, sem, prev_cgpa, gpa):
 
     """
-    Calculate the CGPA given the level, semester, previous CGPA, and current GPA.
-    level: integer representing the student's level
-    sem: integer representing the student's semester (1 or 2)
-    prev_cgpa: float representing the student's previous CGPA
-    gpa: float representing the student's current semester GPA
-    return: float rounded to 2 decimal places representing the CGPA
+    Calculate the CGPA of the Admission Mode "UTME" given the level, semester, previous CGPA, and current GPA.
     """
-    
-
     cgpa = prev_cgpa #initialize cgpa to the previous cgpa
 
     if level == 100:
-
         if sem == 1:
-
             cgpa = 0
-
         elif sem == 2:
-
             cgpa = (prev_cgpa + gpa) / 2
-
     elif level == 200:
-
         if sem == 1:
-
             cgpa = (prev_cgpa * 2 + gpa) / 3
-
         elif sem == 2:
-
             cgpa = (prev_cgpa * 3 + gpa) / 4
-
     elif level == 300:
-
         if sem == 1:
-
             cgpa = (prev_cgpa * 4 + gpa)/5
-
         elif sem == 2:
-
             cgpa = (prev_cgpa * 5 + gpa)/6
-
     elif level == 400:
-
         if  sem == 1:
-
             cgpa = (prev_cgpa * 6 + gpa)/7
-
         elif sem == 2:
-
             cgpa = (prev_cgpa * 7 + gpa)/8
-
     elif level == 500:
-
         if sem == 1:
-
             cgpa = (prev_cgpa * 8 + gpa)/9
-
         elif sem == 2:
-
             cgpa = (prev_cgpa * 9 + gpa)/10
-
     elif level == 600:
-
         if sem == 1:
-
             cgpa = (prev_cgpa * 10 + gpa)/11
-
         elif sem == 2:
-
             cgpa = (prev_cgpa * 11 + gpa)/12
-
     return round(cgpa, 2)
 
+def calculate_cgpa_de(level, sem, prev_cgpa, gpa):
+    """
+    Calculate the CGPA of the admission mode "DE" given the level, semester, previous CGPA, and current GPA.
+    """
+    cgpa = prev_cgpa #initialize cgpa to the previous cgpa
+    if level == 200:
+        if sem == 1:
+            cgpa = 0
+        elif sem == 2:
+            cgpa = (prev_cgpa + gpa) / 2
+    elif level == 300:
+        if sem == 1:
+            cgpa = (prev_cgpa * 2 + gpa) / 3
+        elif sem == 2:
+            cgpa = (prev_cgpa * 3 + gpa) / 4
+    elif level == 400:
+        if sem == 1:
+            cgpa = (prev_cgpa * 4 + gpa)/5
+        elif sem == 2:
+            cgpa = (prev_cgpa * 5 + gpa)/6
+    elif level == 500:
+        if  sem == 1:
+            cgpa = (prev_cgpa * 6 + gpa)/7
+        elif sem == 2:
+            cgpa = (prev_cgpa * 7 + gpa)/8
+    elif level == 600:
+        if sem == 1:
+            cgpa = (prev_cgpa * 8 + gpa)/9
+        elif sem == 2:
+            cgpa = (prev_cgpa * 9 + gpa)/10            
+    return round(cgpa, 2)
+    
 def display_table(course_codes, course_units, grades):
     """
-    Display a table of the course code, course units and course grade.
-    course_codes: list of strings representing the course codes
-    course_units: list of integers representing the course units
-    grades: list of integers representing the grades
+    Display a table of the course codes, course units, and grades.
     """
-    
-
     data = list(zip(course_codes, course_units, grades))
     df = pd.DataFrame(data, columns=['Course Code', 'Course Unit', 'Grade'])
     df.index = df.index + 1
     return df
 
 def generate_result(course_codes, course_units, grades, level, sem, prev_cgpa):
-    
     """
     Generate the student's result and display it in a table.
-    course_codes: list of strings representing the course codes
-    course_units: list of integers representing the course units
-    grades: list of integers representing the grade
-    level: integer representing the student's level
-    sem: integer representing the student's semester (1 or 2)
-    prev_cgpa: float representing the student's previous CGPA
     """
-
-
     gpa = calculate_gpa(course_units, grades)
-    cgpa = calculate_cgpa(level, sem, prev_cgpa, gpa)
     print(display_table(course_codes, course_units, grades))
     print("GPA: ", gpa)
-    print("CGPA: ", cgpa)
-
+    if admission_mode == 'UTME':
+    	cgpa = calculate_cgpa_utme(level, sem, prev_cgpa, gpa)
+    	print("CGPA: ", cgpa)
+    if admission_mode == 'DE':
+    	cgpa = calculate_cgpa_de(level, sem, prev_cgpa, gpa)
+    	print("CGPA: ", cgpa)
+    	    	
+	 
+# Acquire Student admission mode 
+try:
+	 admission_mode = input("'UTME or DE': ").upper()
+	 if admission_mode not in ['UTME', 'DE']:
+	 	 raise ValueError("Invalid Input. Input one of 'UTME' or 'DE'")
+except ValueError as e:
+	 	 print(e)
+	 	 admission_mode = input("'UTME or DE': ")
+	 	 
 # acquire student's level
 try:
     level = int(input("Level: "))
-    if level < 100 or level > 600 or level not in (100, 200, 300, 400, 500, 600):
-        raise ValueError("Invalid input. Please enter a level between 100 - 600.")
+    if admission_mode == 'UTME':
+	    if level < 100 or level > 600 or level not in (100, 200, 300, 400, 500, 600):
+	        raise ValueError("Invalid input. Please enter a level between 100 - 600.")
 except ValueError as e:
-    print(e)
-    level = int(input("Level: "))
+	    print(e)
+	    level = int(input("Level: "))
+	    
+try:	    
+	if admission_mode == 'DE':
+	        if level < 200 or level > 600 or level not in (200, 300, 400, 500, 600):
+	        	raise ValueError("Invalid input. Please enter a level between 200 - 600.")
+except ValueError as e:
+	    print(e)
+	    level = int(input("Level: "))
 
 # acquire student's semester
 try:
@@ -173,8 +172,8 @@ print("===========================")
 # loop to accept course codes, course units, and grades
 for i in range(course_num):
     while True:
-        try:
-            course_code = input("Course code for course {}: ".format(i+1))
+        try:        
+            course_code = input("Course code for course {}: ".format(i+1)).upper()
             course_unit = int(input("Course unit for course {}: ".format(i+1)))
             course_grade = input("Grade for course {}: ".format(i+1)).upper()
             if course_unit < 1 or course_unit > 6:
